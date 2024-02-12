4a345e173bebbbb436badd6e354c34be3bfdb39a
rxjans <120579803+rxjans@users.noreply.github.com>
2024-02-12 18:11:46 +0530

    re: fix: api render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Cards from './components/cards';
 import Nav from './components/nav';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch('https://api.github.com/users');
+    setUsers(await response.json());
+  }
+
+  useEffect(()=>{
+    getUsers();
+  }, []);
+
   return(
     <div className=' bg-[url("https://transparenttextures.com/patterns/crissxcross.png")] h-screen'>
       <Nav />
       <div className='grid grid-cols-3 mt-10 '>
-        <Cards />
+        {
+          users.map((currElem) => {
+            return <Cards name={currElem.login} img={currElem.avatar_url} followers={currElem.followers_url} />
+          }) 
+        }
       </div>
     </div>
   );
